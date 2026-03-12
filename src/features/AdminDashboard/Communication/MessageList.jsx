0f68427f1f5d36310a3cf73a437aaccf879b3a9e
Shawon Reza <shawon.reza.sr81@gmail.com>
2026-03-12 14:44:55 +0600

    12-03-26: Response format fixed

--- src/features/AdminDashboard/Communication/MessageList.jsx
+++ src/features/AdminDashboard/Communication/MessageList.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState, useMemo, memo } from "react";
 import ReactMarkdown from 'react-markdown';
-// import remarkGfm from 'remark-gfm';
+import remarkGfm from 'remark-gfm';
 import { FiThumbsUp, FiThumbsDown, FiDownload, FiFile, FiChevronDown, FiCornerUpRight } from "react-icons/fi";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axiosApi from "../../../service/axiosInstance";
@@ -396,8 +396,8 @@ const MessageList = ({
                         )}
 
                         {/* ................Convert Markdown to HTML................. */}
-                        <div className="text-sm max-w-xs xl:max-w-none break-words whitespace-pre-wrap [&_p]:mb-3 [&_p:last-child]:mb-0">
-                            <ReactMarkdown>{text}</ReactMarkdown>
+                        <div className="text-sm max-w-xs xl:max-w-none break-words whitespace-normal [&_h3]:mt-3 [&_h3]:mb-2 [&_h3]:font-semibold [&_p]:mb-2 [&_p:last-child]:mb-0 [&_ol]:list-decimal [&_ol]:pl-5 [&_ul]:list-disc [&_ul]:pl-5 [&_li]:mb-1 [&_table]:w-full [&_table]:border-collapse [&_th]:border [&_th]:border-gray-300 [&_th]:px-2 [&_th]:py-1 [&_th]:text-left [&_td]:border [&_td]:border-gray-300 [&_td]:px-2 [&_td]:py-1 [&_table]:text-xs sm:[&_table]:text-sm">
+                            <ReactMarkdown remarkPlugins={[remarkGfm]}>{text}</ReactMarkdown>
                             {/* {!isAI && (
                             <p className="break-words">
                                 <ReactMarkdown>{text}</ReactMarkdown>
